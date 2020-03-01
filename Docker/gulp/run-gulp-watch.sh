6e176c178fbb64f61c0f81f40763e1400133c1bc
Alpha01 <root@rubyninja.org>
2020-02-29 18:47:28 -0800

    wtf

--- Docker/gulp/run-gulp-watch.sh
+++ Docker/gulp/run-gulp-watch.sh
@@ -4,9 +4,9 @@ npm install
 
 ./node_modules/gulp/bin/gulp.js build
 
-if [ "$APP_ENV" == "dev" ]; then
+if [ "$APP_ENV" = "dev" ]; then
     ./node_modules/gulp/bin/gulp.js watch
 else
     rm -rf /vendor-assets/*
-    cp -rv /vendor /vendor-assets # Jenkins
+    cp -rv /vendor/ /vendor-assets/ # Jenkins
 fi
