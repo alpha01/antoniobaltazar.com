685959a73c64777b78fe265956b7ffe5112aeff4
Alpha01 <root@rubyninja.org>
2020-03-01 02:06:45 -0800

    fix

--- Docker/varnish/run-varnish.sh
+++ Docker/varnish/run-varnish.sh
@@ -7,9 +7,9 @@ if [ "$?" != "0" ]; then
 fi
 
 if [ "$APP_ENV" = "prod" ]; then
-    $SLEEP_INTERVAL="25"
+    SLEEP_INTERVAL="25"
 else
-    $SLEEP_INTERVAL="10"
+    SLEEP_INTERVAL="10"
 fi
 
 (sleep $SLEEP_INTERVAL && varnishncsa -f /etc/varnish/varnishncsa_formatfile -t off) &
